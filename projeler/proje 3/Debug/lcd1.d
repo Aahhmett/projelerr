ded9849c10a7f1e99c1440736727c2aca8bc1c8e
Aahhmett <ahmet.yildirim23@ogr.sakarya.edu.tr>
2025-01-17 22:53:43 +0300

    projeler

---
+++ projeler/proje 3/Debug/lcd1.d
@@ -0,0 +1,21 @@
+# FIXED
+
+lcd1.obj: ../lcd1.c
+lcd1.obj: C:/Users/alper/workspace_v12/seri\ port\ odev\ videosu/lcd1.h
+lcd1.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h
+lcd1.obj: C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h
+lcd1.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h
+lcd1.obj: C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h
+
+../lcd1.c:
+
+C:/Users/alper/workspace_v12/seri\ port\ odev\ videosu/lcd1.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_memmap.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/inc/hw_types.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/sysctl.h:
+
+C:/ti/TivaWare_C_Series-2.2.0.295/driverlib/gpio.h:
+
